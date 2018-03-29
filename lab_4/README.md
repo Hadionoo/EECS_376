c60d95af8139c72b640b6041afa488c251841746
Hadionoo <hadionoc@gmail.com>
2018-03-29 17:25:08 -0400

    added lab_4 package

---
+++ lab_4/README.md
@@ -0,0 +1,8 @@
+# lab_4
+
+Your description goes here
+
+## Example usage
+
+## Running tests/demos
+    
---
+++ lab_4/src/lab_4_server.cpp
@@ -0,0 +1,90 @@
+// sin_commander node: 
+// node that commands sinusoidal velocities to the minimal controller
+// asks the user for an amplitude and a frequency
+// publishes to the vel_cmd topic
+
+#include <ros/ros.h>
+#include <example_ros_service/PathSrv.h>
+#include <nav_msgs/Path.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/PoseStamped.h>
+
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <actionlib/server/simple_action_server.h>
+#include <lab_4/path_msgAction.h>
+using namespace std;
+
+
+class Lab4Server {
+private:
+
+    ros::NodeHandle nh_;
+    actionlib::SimpleActionServer<lab_4::path_msgAction> as_;
+    lab_4::path_msgGoal goal_;
+    lab_4::path_msgResult result_;
+    lab_4::path_msgFeedback feedback_;
+    
+    //set up variables to check the number of cycles
+    //double numCycles;
+
+
+public:
+   
+    Lab4Server();
+    ~Lab4Server(void){
+    }
+    
+    void executeCB(const actionlib::SimpleActionServer<lab_4::path_msgAction>::GoalConstPtr& goal);
+};    
+    //initializing the object as_
+    //action server will be known by sine_action
+    //this name is what clients use to connect to the server
+    Lab4Server::Lab4Server() :
+    as_(nh_, "path_action", boost::bind(&Lab4Server::executeCB, this, _1),false)
+{
+
+    ROS_INFO("in constructor of Lab4Server...");
+    as_.start();
+}
+
+
+void Lab4Server::executeCB(const actionlib::SimpleActionServer<lab_4::path_msgAction>::GoalConstPtr& goal){
+   ROS_INFO("in executeCB");
+   geometry_msgs::Twist twist_cmd; // creating variable to be published
+   ros::NodeHandle n; // node handle
+   ros::Publisher twist_cmd_publisher = n.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 1);
+   twist_cmd = goal->pose;
+    twist_cmd_publisher.publish(twist_cmd);
+    ROS_INFO("published");
+    as_.setSucceeded(result_);
+
+    if (as_.isPreemptRequested()){	
+          ROS_WARN("goal cancelled!");
+          result_.result_msg = 0;
+          as_.setAborted(result_); // tell the client we have given up on this goal; send the result message as well
+          return; // done with callback
+
+    //feedback_.fdbk = 1; // populate feedback message with current countdown value
+ 	   //as_.publishFeedback(feedback_); // send feedback to the action client that requested this goal   
+     }
+
+    result_.result_msg = 0; //value should be zero, if completed countdown
+   }
+
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "lab_4_server"); //this node will be called sin_commander 
+    Lab4Server as_object;
+    ROS_INFO("going into spin");
+    // do work here in infinite loop (desired for this example), but terminate if detect ROS has faulted
+    while (ros::ok()) 
+    {
+	ros::spinOnce(); 
+    }
+    return 0;
+}
+
+
